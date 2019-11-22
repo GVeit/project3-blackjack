7a81d6ed50555afa1f2841773cec8bce13c1d3ab
Gabriel Veit <gkv8601@rit.edu>
2019-11-22 14:18:22 -0500

    react for addfunds

--- client/blackJack/addFunds.js
+++ client/blackJack/addFunds.js
@@ -3,7 +3,55 @@ let csrfToken;
 // addFunds WINDOW
 const addFundsWindow = (props) => {
     return (
-        <div>It works!</div>
+        <div class="payment">
+            <form>
+                <div class="form-group owner">
+                    <label for="owner">Owner</label>
+                    <input type="text" class="form-control" id="owner">
+                </div>
+                <div class="form-group CVV">
+                    <label for="cvv">CVV</label>
+                    <input type="text" class="form-control" id="cvv">
+                </div>
+                <div class="form-group" id="card-number-field">
+                    <label for="cardNumber">Card Number</label>
+                    <input type="text" class="form-control" id="cardNumber">
+                </div>
+                <div class="form-group" id="expiration-date">
+                    <label>Expiration Date</label>
+                    <select>
+                        <option value="01">January</option>
+                        <option value="02">February </option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                    </select>
+                    <select>
+                        <option value="16"> 2019</option>
+                        <option value="17"> 2020</option>
+                        <option value="18"> 2021</option>
+                        <option value="19"> 2022</option>
+                        <option value="20"> 2023</option>
+                        <option value="21"> 2024</option>
+                    </select>
+                </div>
+                <div class="form-group" id="credit_cards">
+                    <img src="assets/img/visa.jpg" id="visa">
+                    <img src="assets/img/mastercard.jpg" id="mastercard">
+                    <img src="assets/img/amex.jpg" id="amex">
+                </div>
+                <div class="form-group" id="pay-now">
+                    <button type="submit" class="btn btn-default" id="confirm-purchase" style="width: 100%;">Confirm</button>
+                </div>
+            </form>
+        </div>
     );
 };
 
@@ -11,7 +59,7 @@ const addFundsWindow = (props) => {
 const createaddFundsWindow = (csrf) => {
     ReactDOM.render(
         <addFundsWindow csrf={csrf} />,
-        document.querySelector("#game")
+        document.querySelector("#reactCredit")
     );
 };
 
@@ -26,6 +74,11 @@ const getToken = () => {
 $(document).ready(function() {
     getToken();
 });
+const setup = function(csrf) {
+    
+    createaddFundsWindow(csrf);
+
+};
 
 
 const getMoney = (e) =>{
