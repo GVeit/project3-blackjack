a79b417461685e8bf0ca9e26a4ed57a681c9609b
Gabriel Veit <gkv8601@rit.edu>
2019-11-25 14:18:43 -0500

    added react for rules page

---
+++ client/blackJack/rules.js
@@ -0,0 +1,51 @@
+
+const rulesWindow = (props) => {
+    return (
+            
+    <div id="rulesWrapper">
+
+        <h1>Basic Blackjack Rules</h1>
+        
+        <ul style="list-style-type:disc;">
+              <li>The main goal of blackjack is to beat the dealer hand without going over 21.</li>
+              <li>Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.</li>
+              <li>Each player starts with two cards, one of the dealer cards is hidden until the end.</li>
+              <li>To hit is to ask for another card. To stand is to hold your total and end your turn.</li>
+              <li>If you go over 21 you bust, and the dealer wins regardless of the dealer hand.</li>
+              <li>Each player starts with two cards, one of the dealer cards is hidden until the end.</li>
+              <li>If you are dealt 21 from the start (Ace and 10), you got a blackjack.</li>
+              <li>Blackjack usually means you win 2 the amount of your bet.</li>
+              <li>Dealer will hit until their cards total 17 or higher.</li>
+        </ul>  
+        
+    </div>
+    
+    );
+};
+
+const createRulesWindow = (csrf) => {
+    ReactDOM.render(
+        <rulesWindow csrf={csrf} />,
+        document.querySelector("#rules-wrapper")
+    );
+};
+
+/* =+=+=+=+=+=+=+=+=+=+=+= */
+
+const getToken = () => {
+    sendAjax('GET', '/getToken', null, (result) => {
+      csrfToken = result.csrfToken;
+        setup(result.csrfToken);
+    });
+};
+
+$(document).ready(function() {
+    getToken();
+});
+
+
+const setup = function(csrf) {
+    
+    createRulesWindow(csrf);
+
+};
--- hosted/rulesBundle.js
+++ hosted/rulesBundle.js
@@ -1,27 +1,36 @@
+"use strict";
+
+var csrfToken = void 0;
+
 var RulesWindow = function RulesWindow(props) {
     return React.createElement(
-        <h1>Basic Blackjack Rules</h1>
-        
-        <ul style="list-style-type:disc;">
-              <li>The main goal of blackjack is to beat the dealer's hand without going over 21.</li>
-              <li>Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.</li>
-              <li>Each player starts with two cards, one of the dealer's cards is hidden until the end.</li>
-              <li>To 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn.</li>
-              <li>If you go over 21 you bust, and the dealer wins regardless of the dealer's hand.</li>
-              <li>Each player starts with two cards, one of the dealer's cards is hidden until the end.</li>
-              <li>If you are dealt 21 from the start (Ace and 10), you got a blackjack.</li>
-              <li>Blackjack usually means you win 2 the amount of your bet.</li>
-              <li>Dealer will hit until his/her cards total 17 or higher.</li>
-        </ul>  
+        "div",
+        { id: "rules-wrapper" },
+        React.createElement(
+        )
     );
 };
 
+
 var createRulesWindow = function createRulesWindow(csrf) {
-    ReactDOM.render(React.createElement(RulesWindow, { csrf: csrf }), document.querySelector("#rulesWrapper"));
+    ReactDOM.render(React.createElement(RulesWindow, { csrf: csrf }), document.querySelector("#rules-wrapper"));
 };
+
+/* =+=+=+=+=+=+=+=+=+=+=+= */
+
+var getToken = function getToken() {
+    sendAjax('GET', '/getToken', null, function (result) {
+        csrfToken = result.csrfToken;
+        setup(result.csrfToken);
+    });
+};
+
+
+$(document).ready(function () {
+    getToken();
+});
 
 var setup = function setup(csrf) {
 
-    RulesWindow(csrf);
-
+    createRulesWindow(csrf);
 };
