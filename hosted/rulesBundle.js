f3b300e036265a151cb40fae4cfdb976295aa7d8
Gabriel Veit <gkv8601@rit.edu>
2019-11-25 14:36:01 -0500

    added account page

--- hosted/rulesBundle.js
+++ hosted/rulesBundle.js
@@ -34,3 +34,18 @@ var setup = function setup(csrf) {
 
     createRulesWindow(csrf);
 };
+
+var sendAjax = function sendAjax(type, action, data, success) {
+    $.ajax({
+        cache: false,
+        type: type,
+        url: action,
+        data: data,
+        dataType: "json",
+        success: success,
+        error: function error(xhr, status, _error) {
+            var messageObj = JSON.parse(xhr.responseText);
+            handleError(messageObj.error);
+        }
+    });
+};
